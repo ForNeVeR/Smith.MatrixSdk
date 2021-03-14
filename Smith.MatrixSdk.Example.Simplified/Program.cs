b5f42099737970345f4d5e0ee68c7adecc335ea0
Friedrich von Never <friedrich@fornever.me>
2021-03-14 17:34:47 +0700

    Example.Simplified: simplify more

--- Smith.MatrixSdk.Example.Simplified/Program.cs
+++ Smith.MatrixSdk.Example.Simplified/Program.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
+using Smith.MatrixSdk;
 
-namespace Smith.MatrixSdk.Example.Simplified
+static string AskString(string title)
 {
-    public static class Program
-    {
-        private static string AskString(string title)
-        {
-            Console.Write($"{title}: ");
-            return Console.ReadLine()!;
-        }
-
-        public static async Task Main()
-        {
-            var logger = NullLogger.Instance;
-            using var httpClient = new HttpClient();
-            var client = new MatrixClient(logger, httpClient, "https://matrix.org");
-
-            var user = AskString("User");
-            var password = AskString("Password");
-            var loginResponse = await client.Login(user, password);
-
-            Console.WriteLine(loginResponse.AccessToken);
-        }
-    }
+    Console.Write($"{title}: ");
+    return Console.ReadLine()!;
 }
+
+var logger = NullLogger.Instance;
+using var httpClient = new HttpClient();
+var client = new MatrixClient(logger, httpClient, "https://matrix.org");
+
+var user = AskString("User");
+var password = AskString("Password");
+var loginResponse = await client.Login(user, password);
+
+Console.WriteLine($"Access token: {loginResponse.AccessToken}");
